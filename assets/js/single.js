7b07a7dd0511fe663a73876077860010d1276d00
elfsvet <stepanmatysik@gmail.com>
2022-04-13 18:40:27 -0400

    display warning

--- assets/js/single.js
+++ assets/js/single.js
@@ -17,8 +17,10 @@ var getRepoIssues = function (repo) {
                 displayIssues(data);
                 
                 // check if api has paginated issues
+                // This would return the value of Link, if that header exists
                 if (response.headers.get("Link")) {
-                    console.log("repo has more than 30 issues");
+                    // console.log("repo has more than 30 issues");
+                    displayWarning(repo);
                 }
             });
         }
@@ -63,5 +65,20 @@ var displayIssues = function (issues) {
 
     }
 };
+
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+};
+
+
 
 getRepoIssues("facebook/react");
